cc930a57187227913d4681eaed7b971e123fe514
Hassan Ali <hassanali81213@gmail.com>
2022-10-30 14:18:24 +0500

    Added by Hassan Ali

---
+++ PostLab9.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int type;
+    while (1)
+    {
+        scanf("%d",&type);
+        if (type==1 || type==2)
+        {
+            break;
+        }
+    }
+    
+}
